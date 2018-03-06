311ebda45de86a97d673f78b86e0ecba3e6b72f6
arvindnitm@gmail.com <arvindnitm@gmail.com>
2018-03-06 23:17:05 +1100

    add question endpoint with mockdata and livedata endpoint

--- server/index.jsx
+++ server/index.jsx
@@ -1,13 +1,64 @@
 import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
+import webpack from 'webpack';
+import path from 'path';
+
+import { argv } from 'optimist';
+import { get } from 'request-promise';
+import { questions, question } from '../data/api-real-url';
+import { delay } from 'redux-saga';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const useLiveData = argv.userLiveData === 'true';
+
+function* getQuestions() {
+    let data;
+    if (useLiveData) {
+        data = yield get(questions, { gzip: true })
+    } else {
+        data = yield fs.readFile('./data/mock-questions.json', 'utf-8')
+    }
+    return JSON.parse(data);
+}
+function* getQuestion(question_id) {
+    let data;
+    if (useLiveData) {
+        data = yield get(question(question_id), 'utf-8')
+    } else {
+        const questions = yield getQuestions();
+        const question = questions.items.find(_question => _question.question_id == question_id);
+        
+        data= {items: [question]};
+    }
+    return data;
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const config = require('../webpack.config.dev.babel').default;
+    const compiler = webpack(config);
+    app.use(require('webpack-dev-middleware')(compiler, { noInfo: true }))
+    app.use(require('webpack-hot-middleware')(compiler));
+}
+
+app.get('/api/questions', function* (req, res) {
+    const data = yield getQuestions();
+    yield delay(150);
+    res.json(data);
+})
+
+app.get('/api/question/:id', function* (req, res) {
+    const data = yield getQuestion(req.params.id);
+    yield delay(150);
+    res.json(data);
+});
 
 app.get(['/'], function* (req, res) {
     const index = yield fs.readFile('./public/index.html', 'utf-8');
     res.send(index);
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, '0.0.0.0', () => console.info(`App listening on ${port}`));
--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -1,40 +1,45 @@
 import webpack from 'webpack';
 import path from 'path';
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 export default {
+
     entry: [
         'webpack-hot-middleware/client?reload=true',
         'babel-regenerator-runtime',
         path.resolve(__dirname, 'src/')
     ],
+
     output: {
         path: path.resolve(__dirname, 'public'),
         publicPath: '/',
         filename: 'bundle.js'
     },
     plugins: [
+        new webpack.NamedModulesPlugin(), // This will stop cache being invalidated in some scenarios
+        new HtmlWebpackPlugin({template:'client/index.html'}),
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: 'development',
                 WEBPACK: true
             }
-        })
+        }),
+
     ],
     resolve: {
         extensions: ['.js', '.json', '.jsx']
     },
-    module: {
-        loaders: [{
-            test: /\.jsx?/,
-            use: {
-                loader: 'babel-loader'
-            },
-            include: path.resolve(__dirname, 'src')
-        }]
+    optimization: {
+
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        }
     }
-
-
-
 }
